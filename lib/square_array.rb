f74f0430932ee7aa5ddea702c4a2628aa46cd910
Jose Mendoza <pball626@gmail.com>
2021-02-07 20:50:44 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,9 +3,10 @@ def square_array(array)
  count = 0
  new_array = []
  while count < array.length do 
-   array[count] ** 2 new_array.push( array[count] ** 2 )
+   new_array.push( array[count] ** 2 )
    count += 1
  end
+ new_array
  end
  
 # def loop_message_five_times(message)
